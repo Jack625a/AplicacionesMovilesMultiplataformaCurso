9d0e12d1f2d51dd5c6d63050ff763ba361eca268
Jack625a <77822933+Jack625a@users.noreply.github.com>
2024-11-22 21:30:57 -0400

    Layouts, Componentes 2 (Checkox, CupertinoChecbox, Radio, CupertinoRadio, TextField, CupertinoTextField)

--- componentes/main.py
+++ componentes/main.py
@@ -21,10 +21,32 @@ def main(page: ft.Page):
 
 
     )
+    #Funcion para obtener el texto de la entrada de datos textField
+    def textoEntrada(e):
+        value.value=e.control.value
+        page.update()
+    value=ft.Text()
+    #Contenedor para componentes
+    contenidoApp=ft.Column([
+        ft.Text(value="Aplicacion Multiplataforma",size=30,weight=ft.FontWeight.BOLD),
+        ft.CupertinoFilledButton(text="Boton1",icon=ft.icons.FACE),
+        ft.Checkbox(label="Opcion 1"),
+        ft.CupertinoCheckbox(label="Opcion 2"),
+        ft.Radio(label="Opcion 3"),
+        ft.RadioGroup(content=ft.Column([
+            ft.CupertinoRadio(label="Opcion 1", value="Opcion1"),
+            ft.CupertinoRadio(label="Opcion 2", value="Opcion2"),
+            ft.CupertinoRadio(label="Opcion 3", value="Opcion3"),
+            ft.CupertinoRadio(label="Opcion 4", value="Opcion4")
+        ])),
+        ft.TextField(label="Ingrese su nombre",icon=ft.icons.PERSON, on_change=textoEntrada),
+        ft.TextField(label="Ingrese su correo",icon=ft.icons.EMAIL, password=True),
+        ft.CupertinoTextField(placeholder_text="Ingrese su celular"),
+        ft.CupertinoTextField(placeholder_text="Contraseña",password=True)
 
+    ])
 
-
-    page.add(ft.SafeArea(ft.Text("Aplicacion en tiempo real")))
+    page.add(contenidoApp,value)
 
 
 ft.app(main)
